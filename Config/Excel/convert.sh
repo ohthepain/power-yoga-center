fe8581b4b84984542253d3e78e4e9698c187b0f0
Paul Wilkinson <cremoni@gmail.com>
2024-05-03 12:55:33 +0200

    github action - build config

--- Config/Excel/convert.sh
+++ Config/Excel/convert.sh
@@ -3,7 +3,7 @@
 EXCELDIR=`pwd`
 CONFIGDIR="$EXCELDIR/.."
 UTILSDIR="$CONFIGDIR/Utils"
-CONVERT="$UTILSDIR/thriftify.py"
+CONVERT="xl2thrift"
 
-python3 $CONVERT --namespace Yoga.Config --thrift_protocol TJSONProtocol --gen_py ../Thrift/gen-py --output ../../config.bin --class_name "ConfigData"
+$CONVERT --namespace Yoga.Config --thrift_protocol TJSONProtocol --gen_py ../Thrift/gen-py --output ../../config.bin --class_name "ConfigData"
 
